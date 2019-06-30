bdf66f5dde86a80cd4b8fd7b010e4d48d3b8d562
warren <warrenb95@live.co.uk>
2019-06-30 19:23:18 +0100

    added desc to Timespent struct

--- main.go
+++ main.go
@@ -18,6 +18,7 @@ import (
 type Timespent struct {
 	Duration int64     `json:"timespent,omitempty" bson:"timespent,omitempty"`
 	Date     time.Time `json:"timecreated,omitempty" bson:"timecreated,omitempty"`
+	Desc     string    `json:"desc,omitempty" bson:"desc,omitempty"`
 }
 
 type Todo struct {
